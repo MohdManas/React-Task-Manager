cbbf78d6082c5cca4640e8f8f233f8f6bc329b96
Mohd Manas <mohamedmanas2306@gmail.com>
2024-11-26 12:03:04 +0400

    New task committed

---
+++ React/src/components/FormHeader.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+// import {FormHeading} from './components/Styles'
+import {FormHeading} from './Styles'
+const FormHeader = () => {
+  return (
+    <FormHeading>
+      <h1>Task Manager</h1>
+    </FormHeading>
+  );
+};
+
+export default FormHeader;
